086bff283e9580d56c0b22f8b5dfb88d90d49a0b
Jhoan Perez <jhoansebastianperez73@gmail.com>
2023-10-30 18:37:36 -0500

    solve add feature

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -43,7 +43,7 @@ function Form({ nameProduct = '' }) {
     if (nameProduct) {
       updateProduct(nameProduct, product)
     } else {
-      addProduct(nameProduct, product)
+      addProduct(product)
     }
 
     setName('')
